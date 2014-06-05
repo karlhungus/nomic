8e06f1a62b36b84ca64e4f92f075d844f8be8a02
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-05 11:05:09 -0400

    just puts the params

--- nomic.rb
+++ nomic.rb
@@ -3,8 +3,9 @@ require 'json'
 
 class Nomic < Sinatra::Base
   post '/payload' do
-    push = JSON.parse(params[:payload])
-    puts "I got some JSON: #{push.inspect}"
+    #push = JSON.parse(params[:payload])
+    #puts "I got some JSON: #{push.inspect}"
+    puts "Request #{params.inspect}"
   end
   get '/' do
       'Hello world!'
