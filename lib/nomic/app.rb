f1f62f32ca421f151717bda1e1c1c9285355e246
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-05 17:19:06 +0000

    print out last request

--- lib/nomic/app.rb
+++ lib/nomic/app.rb
@@ -2,18 +2,27 @@ require 'sinatra'
 require 'haml'
 require 'nomic'
 require 'json'
+require 'byebug'
 
 class Nomic::App < Sinatra::Base
   use Rack::CommonLogger
   set :views, File.join(Nomic::ROOT_PATH, "views")
   set :public, File.join(Nomic::ROOT_PATH, "public")
 
+  @@data = {request: "no request"}
   post '/payload' do
-    data = JSON.parse request.body.read
-    data.to_json.to_s
+    @@data = JSON.parse request.body.read
+
+    case request.env['HTTP_X_GITHUB_EVENT']
+    when 'pull_request'
+        { "mission" => "pull request success" }.to_s
+    when 'pull_request_review_comment'
+        { "mission" => "success" }.to_s
+    end
   end
 
   get '/' do
+    @data = @@data
     haml :index
   end
 end
