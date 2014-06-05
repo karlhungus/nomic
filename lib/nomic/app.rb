7db85ae390bf56995f2dd85bfefc8d4cddcdde47
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-05 11:13:40 -0400

    remove old stuff now that nomic lives in lib

--- lib/nomic/app.rb
+++ lib/nomic/app.rb
@@ -3,13 +3,17 @@ require 'haml'
 require 'nomic'
 
 class Nomic::App < Sinatra::Base
+  use Rack::CommonLogger
+  set :views, File.join(Nomic::ROOT_PATH, "views")
+  set :public, File.join(Nomic::ROOT_PATH, "public")
 
-    use Rack::CommonLogger
+  post '/payload' do
+    #push = JSON.parse(params[:payload])
+    #puts "I got some JSON: #{push.inspect}"
+    puts "Request #{params.inspect}"
+  end
 
-    set :views, File.join(Nomic::ROOT_PATH, "views")
-    set :public, File.join(Nomic::ROOT_PATH, "public")
-
-    get '/' do
-        haml :index
-    end
+  get '/' do
+    haml :index
+  end
 end
