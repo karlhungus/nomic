a720d5c1e7943c393be0d0bb8f902a34463e585d
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-06 10:08:28 -0400

    merge on rule success

--- lib/github_helper.rb
+++ lib/github_helper.rb
@@ -3,15 +3,14 @@ module GithubHelper
     #if Rails.env.test?
     #  Octokit::Client.new :access_token => ENV['TEST_API_TOKEN']
     #else
-      Octokit::Client.new :access_token => current_user.token
+    #  Octokit::Client.new :access_token => current_user.token
     #end
+    Octokit::Clinet.new(client_id: '007deba9b949a9fb1810', client_secret: '6458ea636e4ffaaf1e2a1c70d34fb554053847ab')
   end
 
   def github_client
     unless @client
       @client = prepare_github
-      @gh_user = @client.user
-      @gh_user.login
     end
     @client
   end
