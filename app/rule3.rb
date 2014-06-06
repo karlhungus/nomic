fbaa952304d68134dc38e170df6794c99f4d3712
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-06 13:48:46 -0400

    little update to rule3

--- app/rule3.rb
+++ app/rule3.rb
@@ -1,26 +1,10 @@
+require 'bad_word_detector'
+
 class Rule3 < Nomic::Rule
   include GithubHelper
 
   def name
     "Pass if the comments would be appropriate in Sunday school"
-  end
-
-  def repo_name
-    @issue_comment['repository']['full_name']
-  end
-
-  def issue_number
-    @issue_comment["issue"]["number"]
-  end
-
-  def issue_comments
-    github_client.issue_comments(repo_name, issue_number)
-  end
-
-  def last_comments
-    issue_comments.group_by{|c| c[:user][:id]}.values.map do |user_comments|
-      user_comments.max_by{|comment| comment[:updated_at]}
-    end
   end
 
   def approval?(comment)
