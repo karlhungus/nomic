3311cc12d8021c5b648ac05bed9f5835999c3b6b
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-06 13:22:48 -0400

    add tokens so heroku can work

--- lib/nomic.rb
+++ lib/nomic.rb
@@ -1,21 +1,21 @@
 module Nomic
- extend self
- ROOT_PATH = File.expand_path("..", File.dirname(__FILE__))
- CONFIG_PATH = File.join(ROOT_PATH,'config')
+  extend self
+  ROOT_PATH = File.expand_path("..", File.dirname(__FILE__))
+  CONFIG_PATH = File.join(ROOT_PATH,'config')
 
- def github_token
-  @github_token ||= load_config['github']['api_key']
- end
+  def github_token
+    @github_token ||= load_config['github']['api_key']
+  end
 
- def heroku_token
-     @heroku_token ||= load_config['heroku']['api_key']
- end
+  def heroku_token
+    @heroku_token ||= load_config['heroku']['api_key']
+  end
 
- private 
- def load_config
-     @config ||= begin 
-                     filename = File.join(CONFIG_PATH, "tokens.yml")
-                     YAML.load(File.read(filename))
-                 end
- end
+  private
+  def load_config
+    @config ||= begin
+      filename = File.join(CONFIG_PATH, "tokens.yml")
+      YAML.load(File.read(filename))
+    end
+  end
 end
