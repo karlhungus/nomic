88d7bac22580b96520fde93f5d84b960fdef5ee2
Ben Doyle <ben.doyle@shopify.com>
2014-06-06 13:08:22 -0400

    publish rules infrastructure

--- app/rule1.rb
+++ app/rule1.rb
@@ -7,29 +7,12 @@ class Rule1 < Nomic::Rule
     "Pass if there are sufficient upvotes"
   end
 
-  def repo_name
-    @issue_comment['repository']['full_name']
-  end
-
-  def issue_number
-    @issue_comment["issue"]["number"]
-  end
-
-  def issue_comments
-    github_client.issue_comments(repo_name, issue_number)
-  end
-
-  def last_comments
-    issue_comments.group_by{|c| c[:user][:id]}.values.map do |user_comments|
-      user_comments.max_by{|comment| comment[:updated_at]}
-    end
-  end
-
-  def approval?(comment)
+  def approval_comment?(comment)
     comment.include? APPROVAL_STRING
   end
 
   def pass
-    last_comments.select{|comment| approval?(comment[:body])}.size >= MIN_APPROVALS
+    debugger
+    last_comments.select{|comment| approval_comment?(comment[:body])}.size >= MIN_APPROVALS
   end
 end
