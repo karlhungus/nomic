9e0982d2842c0e8b0cd740d801eccf83139aa2a6
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-10 21:30:26 -0400

    update nomic to use env variables

--- lib/nomic.rb
+++ lib/nomic.rb
@@ -1,30 +1,19 @@
 module Nomic
   extend self
-  ROOT_PATH = File.expand_path("..", File.dirname(__FILE__))
-  CONFIG_PATH = File.join(ROOT_PATH,'config')
 
   def github_token
-    @github_token ||= load_config['github']['api_key']
+    @github_token ||= ENV[GITHUB_API_KEY]
   end
 
   def github_repo
-    @github_repo ||= load_config['github']['repo_name']
+    @github_repo ||= ENV[GITHUB_REPO_NAME]
   end
 
   def heroku_token
-    @heroku_token ||= load_config['heroku']['api_key']
+    @heroku_token ||= ENV[HEROKU_API_KEY]
   end
 
   def heroku_app_name
-    @heroku_token ||= load_config['heroku']['api_name']
-  end
-
-  private
-
-  def load_config
-    @config ||= begin
-      filename = File.join(CONFIG_PATH, "tokens.yml")
-      YAML.load(File.read(filename))
-    end
+    @heroku_token ||= ENV[HEROKU_APP_NAME]
   end
 end
