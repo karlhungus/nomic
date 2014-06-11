1d4a904e09c7829b9bde571dbf48c48655ae760a
Izaak Alpert <izaak.alpert@shopify.com>
2014-06-10 22:05:41 -0400

    fix tokens

--- lib/nomic.rb
+++ lib/nomic.rb
@@ -3,18 +3,18 @@ module Nomic
   ROOT_PATH = File.expand_path("..", File.dirname(__FILE__))
 
   def github_token
-    @github_token ||= ENV[GITHUB_API_KEY]
+    @github_token ||= ENV['GITHUB_API_KEY']
   end
 
   def github_repo
-    @github_repo ||= ENV[GITHUB_REPO_NAME]
+    @github_repo ||= ENV['GITHUB_REPO_NAME']
   end
 
   def heroku_token
-    @heroku_token ||= ENV[HEROKU_API_KEY]
+    @heroku_token ||= ENV['HEROKU_API_KEY']
   end
 
   def heroku_app_name
-    @heroku_token ||= ENV[HEROKU_APP_NAME]
+    @heroku_token ||= ENV['HEROKU_APP_NAME']
   end
 end
